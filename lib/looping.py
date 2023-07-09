924cca43a6662fff82abe77431f90a28b507805a
Justin Bigishiro <justin.rvd@gmail.com>
2023-07-09 06:22:06 -0500

    adds my file

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    num=10
+    while num>0:
+        print(num)
+        num-=1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+   return [num**2 for num in int_list]
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num=1
+    while num <= 100:
+        if num % 5 == 0 and num % 3 == 0 :
+            print("FizzBuzz")
+        elif num % 5 ==0:
+            print("Buzz")
+        elif num % 3 ==0:
+            print("Fizz")
+        else:
+            print(num)
+        num+=1
